011c5352fe938362fa701c148d930880bed19355
Marianni1 <123604563+Marianni1@users.noreply.github.com>
2024-04-14 18:48:58 -0400

    Add files via upload

--- pruebak6.js
+++ pruebak6.js
@@ -3,14 +3,41 @@ import { sleep, check } from 'k6';
 
 export let options = {
   vus: 100, 
-  duration: '1m',
+  duration: '2m', 
 };
 
 export default function () {
-  let response = http.get('https://recetas-del-campo1.odoo.com/');
-  check(response, {
-    'Status is 200': (r) => r.status === 200,
+ 
+  let loginResponse = http.post('https://recetas-del-campo1.odoo.com/web/login', {
+    login: '',
+    password: '', //se eliminó el usuario y el password por seguridad
   });
 
-  sleep(3);
+  check(loginResponse, {
+    'Inicio de sesión exitoso': (res) => res.status === 200,
+  });
+
+
+    // Agregar producto al carrito 
+    let addToCartResponse = http.post('https://recetas-del-campo1.odoo.com/shop/cart', {
+      product_id: '1',
+      add_qty: 1,
+    }, { cookies: loginResponse.cookies });
+  
+    check(addToCartResponse, {
+      'Producto agregado al carrito': (res) => res.status === 200,
+    });
+  
+    // Realizar el pago
+    let paymentResponse = http.post('https://recetas-del-campo1.odoo.com/shop/payment', {
+      csrf_token: addToCartResponse.json('csrf_token'),
+      payment_method: 'wire_transfer', 
+    }, { cookies: loginResponse.cookies });
+  
+    check(paymentResponse, {
+      'Pago realizado con éxito': (res) => res.status === 200,
+    });
+  
+
+    sleep(3);
 }
